7d48cf7d668e2586b8b0640c6187e2d8f2163a36
dustinjhenderson <dustinjhenderson@gmail.com>
2017-09-07 13:18:02 -0700

    Update Comments

--- downloadParFile.py
+++ downloadParFile.py
@@ -1,16 +1,26 @@
-#download par files from the design store
+#Download .par Files from the design store
+#This def acepts an url to a .par file and downloads the file 
+#to the directory the script is stored in. If the download
+#fials the def will attempt to reDownload the file 3 times
+#before reporting and error and completing
 #Dustin Henderson
-import urllib
 
-def downloadParFile(url, name):
-	attempts = 0
-	while(attempts < 2):
+import urllib	#need to be imported in main script
+
+#Url need to be a url to the .par file download
+#The url entered should be parced from the download button
+#in the design store html code
+#The name need to be a string. The string does not include .par
+#extention. The .par file is downloaded and saved under the name given
+def downloadParFile(url, name):	
+	attempts = 0	#used to store the number of attempts at downloading the file
+	while(attempts < 2): #do not try and download a file more than 3 times
 		try:
-			urllib.urlretrieve (url, name + ".par")
-			break
+			urllib.urlretrieve (url, name + ".par") #retrive the url
+			attempts = 5	#if its sucessful break the loop
 		except:
-			print "Attempts at downloading par: ", attempts
-		attempts = attempts + 1
+			print "Attempts at downloading par: ", attempts	#if there is a fialure print the number of attempts
+		attempts = attempts + 1	#increment the number of attempts
 	
 '''Test Call'''
 #downloadParFile("https://cloud.altera.com/devstore/platform/2008/download/", "test1")
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#main
+#Design Store Script
 #Dustin Henderson
 
-print "Starting Up..."
+print "Starting Up..."	#set up takes some time because the interpeter has to locate and link several different python files
 
+#**** NOTE: ****
+# any imports used in other python defs and classes need to also be imported here in order for the script to exicute properly
+#***************
 try:
 	import os
 except ImportError :
@@ -34,6 +37,7 @@ except ImportError :
 	print 'ERROR : Run the batch file again \nOR \nMake sure \'requests\' package is installed correctly, to check enter in command prompt: \n<python -m pip install openpyxl> and re-run the script'
 	exit()
 
+#import defs and classes from other python files
 from getFileLocation import getFileLocation
 from getCsvName import getNameCsv
 from convertCsv import convertCsv
@@ -41,29 +45,32 @@ from getDocumentationLinks import getDocLinks
 from condenceUrls import condenceUrls
 from analyticsCopy import runAnalytics
 
-csvFileLocation = ""	#stores the file location of the input csv
-csvFileName = ""		#stores the tested name and location of the csv
-xlsFileName = ""		#stores the tested name and location of the xlsx file that is generated from the csv
-csvConvertBool = False	#used to determine if the CSV converted correctly
-convertCsvName = "csvConvert.xlsx" #name of the output xlsx file
-docLinks = []			#stores the urls of the documentation links
+csvFileLocation = ""				#stores the file location of the input csv
+csvFileName = ""					#stores the tested name and location of the csv
+xlsFileName = ""					#stores the tested name and location of the xlsx file that is generated from the csv
+csvConvertBool = False				#used to determine if the CSV converted correctly
+convertCsvName = "csvConvert.xlsx" 	#name of the output xlsx file
+docLinks = []						#stores the urls of the documentation links
 
+#loop asking for file location and CSV name untill valid answers are given
 while(csvConvertBool == False):
 	#get the location of the csv file and check that it exsists
+	#this function loops its self until a valid location is given
 	csvFileLocation = getFileLocation("Enter the location of the CSV file", "location: ")
 
 	#get the name of the csv file and check that it exsists
 	csvFileName = getNameCsv(csvFileLocation, "Enter CSV file name", "Name: ")
 
-	#convirts the csv to xlsx and stores it in the same location
-	print "Converting the CSV..."
-	csvConvertBool = convertCsv(csvFileName, convertCsvName)
+	#convirts the csv to xlsx and stores it in the same location this is for the scripts that use xlsx over csv and viceVersa
+	print "Converting the CSV..."	#comment for testing 
+	csvConvertBool = convertCsv(csvFileName, convertCsvName)	
 
-print "Convert Done"
+print "Convert Done"	#Alert the user that the convertion of the csv file is completed
 
 #get the documentation links
-print "Geting Documentation Links"
-docLinks = getDocLinks(csvFileName)
+print "Geting Documentation Links"	#alert the user that the script is grabbing the documentation links
+docLinks = getDocLinks(csvFileName)	#This function parses the csv file for documentation links
+									#The function returns a multiDemensional array of documentation links
 
 ''' For Testing '''
 print "*****************************************************************************************"
@@ -80,8 +87,9 @@ print "*************************************************************************
 print "************************************After************************************************"
 print "*****************************************************************************************"
 '''End of testing section'''
-print "Checking for duplicate links"
-docLinks = condenceUrls(docLinks)
+print "Checking for duplicate links"	#alter the user that the script is removing duplicate links for the documentation links
+docLinks = condenceUrls(docLinks)		#The function takes in the multiDimentional arry created by the getDocLinks def and
+										#reutns the same dimensioned arry with duplicate links removed
 
 '''Testing Section'''
 testCount = 0
@@ -102,7 +110,8 @@ print "before len: ", testLenBefore, "after len: ", testLenAfter
 '''*********************************************************************************************
 ************************************Next Step is to report anylitics****************************
 *********************************************************************************************'''
-runAnalytics()
+runAnalytics()	#This script opens the .xlsx file. Performs anaylitics calculations
+				#stores the data in the same .xlsx file and saves/closes it
 
 
 '''*********************************************************************************************
@@ -115,5 +124,5 @@ runAnalytics()
 #uploadLast
 
 
-print "Done!"
-exit()
+print "Done!"	#When the script is sucessfuly completed alert the user that it is finished
+exit()			#exit the script
